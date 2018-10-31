e74edf8f998e23ac7c6690e144acec654d2bab88
Ashok Sennan Admin <ashoksennan426@gmail.com>
2018-10-31 10:17:03 +0530

    Solid waste

---
+++ app/src/main/java/com/example/admin/solidwaste/Interface/IShowDialogContract.java
@@ -0,0 +1,18 @@
+package com.example.admin.solidwaste.Interface;
+
+
+
+
+
+import com.example.admin.solidwaste.pojo.UserProductPojo.UserProductResponseResponse;
+
+import java.util.List;
+
+public interface IShowDialogContract {
+
+//    void showDialog(int position);
+
+    void showDialogd(int position, List<UserProductResponseResponse> userProductResponseResponses);
+    void showDeleteDialog(int position, String title);
+
+}
---
+++ app/src/main/java/com/example/admin/solidwaste/pojo/ProductRegistrationPojo/Add_Product_Response.java
@@ -0,0 +1,22 @@
+package com.example.admin.solidwaste.pojo.ProductRegistrationPojo;
+
+public class Add_Product_Response {
+    private String Message;
+    private int StatusCode;
+
+    public String getMessage() {
+        return this.Message;
+    }
+
+    public void setMessage(String Message) {
+        this.Message = Message;
+    }
+
+    public int getStatusCode() {
+        return this.StatusCode;
+    }
+
+    public void setStatusCode(int StatusCode) {
+        this.StatusCode = StatusCode;
+    }
+}
